d8b0499c4e3deaf6c78f5a5f4352e2fc5bc9bd76
matthewwen <mattwen2018@gmail.com>
2018-11-13 14:25:27 -0500

    added objects to python

--- Runtime.py
+++ Runtime.py
@@ -1,16 +1,45 @@
 class Matrix(object):
-    
-    variable = [[1]]
 
-    # The class "constructor" - It's actually an initializer 
-    def _init_(row, col):
-        for i in range(row): 
-            temp = [] 
+    # The class "constructor" - It's actually an initializer
+    def __init__(self, row, col):
+        self.variable = []
+        for i in range(row):
+            temp = []
             for j in range (col):
                 temp.append(0)
-            variable.append(temp)
+            self.variable.append(temp)
 
-    # This displays all the elements in the array
+    def get_variable(self):
+        return self.variable
 
-print("Hello There")
+    def enter_row(self, pos, row):
+        self.variable[pos] = row;
 
+    def enter_col(self, pos, col):
+        for i in range(len(self.variable)):
+            self.variable[i][pos] = col[i]
+
+class NeuroNetwork(object):
+
+    # The class "constructor" - contains all the weights
+    def __init__(self, abs):
+        for i in range(abs):
+            temp = []
+            for j in range (abs):
+                temp.append(uniform(-500, 500))
+            self.weights.append(temp)
+
+    #put a vector inside a neuro network
+    def put_vector(self, level, pos, v):
+        self.v[level][pos] = v;
+
+    #fill neuro network with correct matrix
+    def fill_vector(self):
+        #starting at the top going downward 
+
+
+matrix = Matrix(3,3)
+val = [1,2,3]
+matrix.enter_col(0, val)
+
+print(matrix.get_variable());
